4ab026fa55c964354689dcccb08b05f8ee857a8d
Danick Triantis <danick@triantis.nl>
2024-05-31 21:28:14 +0200

    Removed custom window keymaps.

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -9,9 +9,6 @@ vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagn
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Window Management
-vim.keymap.set("n", "wh", "<C-w>s", { desc = "Split window horizontally" })
-vim.keymap.set("n", "wv", "<C-w>v", { desc = "Split window vertically" })
-
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
