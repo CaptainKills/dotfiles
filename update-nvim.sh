27da3811fa8e6e40c941a8b423b4ee56fb6586c1
Danick Triantis <danick@triantis.nl>
2024-12-04 12:28:42 +0100

    Deleted Nvim update script. Task now handled by Ansible

--- update-nvim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-sudo rm nvim-linux64.tar.gz
-
