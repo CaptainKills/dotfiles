e80c99719d6412aea43235225f41cfde705d6521
Danick Triantis <danick@triantis.nl>
2024-12-08 11:12:42 +0100

    C keymap is now consistent with general keymaps.

--- nvim/ftplugin/c.lua
+++ nvim/ftplugin/c.lua
@@ -1,3 +1,3 @@
 -- Vim Keymaps: C
-vim.keymap.set("n", "<C-S>", ":ClangdSwitchSourceHeader<CR>", { desc = "C: Toggle between source and header file" })
-vim.keymap.set("n", "<C-I>", ":ClangdShowSymbolInfo<CR>", { desc = "C: Show symbol info" })
+vim.keymap.set("n", "<C-s>", ":ClangdSwitchSourceHeader<CR>", { desc = "C: Toggle between source and header file" })
+vim.keymap.set("n", "<C-i>", ":ClangdShowSymbolInfo<CR>", { desc = "C: Show symbol info" })
