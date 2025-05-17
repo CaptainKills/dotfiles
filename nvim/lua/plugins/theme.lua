2e7970f1d849050d2cddcb9671ebfd83f37b5fc3
Danick Triantis <danick@triantis.nl>
2025-05-17 18:50:17 +0200

    Change theme border colour to make better distinction between editors.

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -3,6 +3,11 @@ return {
 	priority = 1000,
 
 	init = function()
+		require("tokyonight").setup({
+			on_colors = function(colors)
+				colors.border = colors.fg_dark
+			end
+		})
 		vim.cmd.colorscheme("tokyonight-night")
 	end,
 }
