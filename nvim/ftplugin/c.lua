687d14b023928731a25ee3a2d1ca170ee6cf8c84
Danick Triantis <danick@triantis.nl>
2025-05-17 18:49:47 +0200

    Updated Clangd functions

--- nvim/ftplugin/c.lua
+++ nvim/ftplugin/c.lua
@@ -1,3 +1,3 @@
 -- Vim Keymaps: C
-vim.keymap.set("n", "<C-s>", ":ClangdSwitchSourceHeader<CR>", { desc = "C: Toggle between source and header file" })
-vim.keymap.set("n", "<C-i>", ":ClangdShowSymbolInfo<CR>", { desc = "C: Show symbol info" })
+vim.keymap.set("n", "<C-s>", ":LspClangdSwitchSourceHeader<CR>", { desc = "C: Toggle between source and header file" })
+vim.keymap.set("n", "<C-i>", ":LspClangdShowSymbolInfo<CR>", { desc = "C: Show symbol info" })
