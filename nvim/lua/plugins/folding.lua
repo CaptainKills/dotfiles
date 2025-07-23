4f687adf67960a7f274222dd574438da70ec740a
Danick Triantis <danick@triantis.nl>
2025-07-23 11:29:46 +0200

    nvim: disabled fold on buffer enter.

--- nvim/lua/plugins/folding.lua
+++ nvim/lua/plugins/folding.lua
@@ -8,27 +8,26 @@ return {
 
 		config = function()
 			local ufo = require("ufo")
+			ufo.setup()
 
 			vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Folding: Open all folds" })
 			vim.keymap.set("n", "zM", ufo.closeAllFolds, { desc = "Folding: Close all folds" })
 			vim.keymap.set("n", "zK", ufo.peekFoldedLinesUnderCursor, { desc = "Folding: Peek folded lines" })
 
-			vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
-				pattern = "?*",
-				group = vim.api.nvim_create_augroup("remember_folds", { clear = true }),
-				callback = function()
-					vim.cmd("mkview")
-				end,
-			})
-			vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-				pattern = "?*",
-				group = vim.api.nvim_create_augroup("remember_folds", { clear = false }),
-				callback = function()
-					vim.cmd("loadview")
-				end,
-			})
-
-			ufo.setup()
+			-- vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+			-- 	pattern = "?*",
+			-- 	group = vim.api.nvim_create_augroup("remember_folds", { clear = true }),
+			-- 	callback = function()
+			-- 		vim.cmd("mkview")
+			-- 	end,
+			-- })
+			-- vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+			-- 	pattern = "?*",
+			-- 	group = vim.api.nvim_create_augroup("remember_folds", { clear = false }),
+			-- 	callback = function()
+			-- 		vim.cmd("loadview")
+			-- 	end,
+			-- })
 		end,
 	},
 
