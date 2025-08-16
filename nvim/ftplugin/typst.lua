94aa5d63be88812710763b97f5862de10d1874cf
Danick Triantis <danick@triantis.nl>
2025-08-16 17:40:48 +0200

    nvim: helper functions to select main file.

--- nvim/ftplugin/typst.lua
+++ nvim/ftplugin/typst.lua
@@ -14,8 +14,45 @@ vim.keymap.set("n", "<leader>lj", ":LspTinymistExportPng<CR>", { desc = "Typst: 
 vim.keymap.set("n", "<leader>ls", ":LspTinymistExportSvg<CR>", { desc = "Typst: Export Svg" })
 
 -- INFO: Typst Preview
-vim.keymap.set("n", "<leader>lp", function()
+local preview_start = function()
+	-- Start Typst Preview
 	vim.cmd("TypstPreview")
-end, { desc = "Typst: Preview Document" })
-vim.keymap.set("n", "<leader>lq", ":TypstPreviewStop<CR>", { desc = "Typst: Stop Preview" })
-vim.keymap.set("n", "<leader>lc", ":TypstPreviewSyncCursor<CR>", { desc = "Typst: Sync Cursor" })
+
+	-- Pin previewed file as main
+	local bufnr = vim.api.nvim_get_current_buf()
+	local buffer = vim.api.nvim_buf_get_name(bufnr)
+	local clients = vim.iter(vim.lsp.get_clients())
+
+	for lsp in clients do
+		if lsp.name == "tinymist" then
+			lsp:exec_cmd({
+				title = "pin",
+				command = "tinymist.pinMain",
+				arguments = { buffer },
+			}, { bufnr = bufnr })
+		end
+	end
+end
+
+local preview_stop = function()
+	-- Stop Typst Preview
+	vim.cmd("TypstPreviewStop")
+
+	-- Unpin previewed file as main
+	local bufnr = vim.api.nvim_get_current_buf()
+	local clients = vim.iter(vim.lsp.get_clients())
+
+	for lsp in clients do
+		if lsp.name == "tinymist" then
+			lsp:exec_cmd({
+				title = "unpin",
+				command = "tinymist.pinMain",
+				arguments = { vim.v.null },
+			}, { bufnr = bufnr })
+		end
+	end
+end
+
+vim.keymap.set("n", "<leader>lp", preview_start, { desc = "Typst: Start Preview Document" })
+vim.keymap.set("n", "<leader>lq", preview_stop, { desc = "Typst: Stop Preview Document" })
+vim.keymap.set("n", "<leader>lc", ":TypstPreviewSyncCursor<CR>", { desc = "Typst: Sync Cursor with Preview" })
