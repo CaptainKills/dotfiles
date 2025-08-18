6c7afc038ea7bd854242a720ea3cdb43a85e1228
Danick Triantis <danick@triantis.nl>
2025-08-18 17:50:00 +0200

    nvim: add condition for Typst math mode for snippets.

--- nvim/luasnippets/typst.lua
+++ nvim/luasnippets/typst.lua
@@ -19,6 +19,25 @@ local placement = function()
 	return sn(nil, c(1, { t("none"), t("auto"), t("top"), t("bottom") }))
 end
 
+local math_mode = function()
+	local ts = require("nvim-treesitter.ts_utils")
+	local node = ts.get_node_at_cursor()
+
+	print("Node: ", node, ", Type: ", node:type(), ", Parent: ", node:parent())
+	if node:type() == "math" then
+		return true
+	end
+
+	local parent = node:parent()
+	if parent then
+		if parent:type() == "math" then
+			return true
+		end
+	end
+
+	return false
+end
+
 return {
 	-- stylua: ignore start
 
@@ -66,12 +85,12 @@ return {
 	}),
 
 	-- Expend _ to _()
-	s({ trig = "_", snippetType = "autosnippet", wordTrig = false }, {
+	s({ trig = "_", snippetType = "autosnippet", wordTrig = false, condition = math_mode }, {
 		t("_("), i(0), t(")"),
 	}),
 
 	-- Expand ^ to ^()
-	s({ trig = "^", snippetType = "autosnippet", wordTrig = false }, {
+	s({ trig = "^", snippetType = "autosnippet", wordTrig = false, condition = math_mode }, {
 		t("^("), i(0), t(")"),
 	}),
 
