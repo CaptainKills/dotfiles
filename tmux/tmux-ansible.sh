1878807f0d37a55a8fcd54f6f51c0fe845054c6b
Danick Triantis <danick@triantis.nl>
2025-09-08 15:01:50 +0200

    tmux: ansible now uses virtual environment properly.

--- tmux/tmux-ansible.sh
+++ tmux/tmux-ansible.sh
@@ -14,13 +14,12 @@ tmux new-session -d -s $SESSION -c $WORKING_DIRECTORY
 
 # Create Nvim Window
 tmux rename-window -t $SESSION:1 "nvim"
+tmux send-keys -t $SESSION:1 "source .venv/bin/activate && clear" C-m
 tmux send-keys -t $SESSION:1 "nvim" C-m
 
 # Create Terminal Window
 tmux new-window -t $SESSION:2 -c $WORKING_DIRECTORY
 tmux rename-window -t $SESSION:2 "terminal"
-
-# Enable Virtual Environment
 tmux send-keys -t $SESSION:2 "source .venv/bin/activate && clear" C-m
 
 # Attach Session
